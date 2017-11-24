0484d9b16140aa0b1cef72b732ddc3e545e06172
Priyanka Negi <prinegi100@gmail.com>
2017-11-25 00:21:06 +0530

    created action for /POST request

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 const BASE_URL = 'https://reduxblog.herokuapp.com/api';
 // const API_KEY = '?key=PAPERCLIP1234';
 export const FETCH_POSTS = 'fetch_posts';
+export const CREATE_POST = 'create_post';
 // const AUTH_TOKEN = ''; 
 
 export function fetchPosts(){
@@ -11,4 +12,13 @@ export function fetchPosts(){
     type: FETCH_POSTS,
     payload: request,
   };
+}
+
+export function createPost(values){
+  const request = axios.post(`${BASE_URL}/posts`, values);
+
+  return{
+    type: CREATE_POST,
+    payload: request
+  }
 }
