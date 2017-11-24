7f711783997d3c94e92aeeb7de4b9b14aaf1b06c
Priyanka Negi <prinegi100@gmail.com>
2017-11-25 00:28:00 +0530

    completed PostsNew  component with validations

--- src/components/PostsNew.js
+++ src/components/PostsNew.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form';
 import { Link } from 'react-router-dom';
-
+import { connect } from 'react-redux';
+import { createPost } from '../actions'
 class PostsNew extends Component{
 
   renderField(field){
@@ -22,6 +23,7 @@ class PostsNew extends Component{
 
 onSubmit(values) {
   console.log(values);
+  this.props.createPost(values);
 
 }
   render(){
@@ -63,4 +65,7 @@ function validate(values) {
 export default reduxForm({
   validate: validate,
   form: 'PostsNewForm'
-})(PostsNew);
+})(
+  connect(null, { createPost })(PostsNew)
+);
+
