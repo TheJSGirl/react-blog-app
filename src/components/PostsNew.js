b21a293a31dcafdf130201cac333e62d83f85a17
Priyanka Negi <prinegi100@gmail.com>
2017-11-25 00:12:42 +0530

    added cancel button

--- src/components/PostsNew.js
+++ src/components/PostsNew.js
@@ -1,17 +1,21 @@
 import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form';
+import { Link } from 'react-router-dom';
 
 class PostsNew extends Component{
 
   renderField(field){
     return (
       <div className = "row">
-        <div className ="input-field col s12 l10">
+        <div className ="input-field col s12 l10 ">
           <h3>{field.label}</h3>
-          <input type="text"  {...field.input} />  
+          <input type="text"  {...field.input}  />  
           {/* <label>{field.label}</label>       */}
         </div> 
-        {field.meta.touched ? field.meta.error :''}
+        <div className="red-text">
+          {field.meta.touched ? field.meta.error : ''}          
+        </div>
+        
       </div>
     )
   }
@@ -27,7 +31,8 @@ onSubmit(values) {
         <Field name="title" component = {this.renderField} label="Title"/>
         <Field name="category" component = {this.renderField} label="category"/>
         <Field name="content" component = {this.renderField} label="Post Content"/>    
-        <button type="submit" className="waves-effect waves-light btn blue">Submit</button>            
+        <button type="submit" className="waves-effect waves-light btn blue">Submit</button> &nbsp;
+        <Link to='/posts' className="waves-effect waves-light btn red">Cancel</Link>   
       </form>
     );
   }
